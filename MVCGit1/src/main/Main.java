6f7b140157b1876eabeaf46ba361dd85df71913f
Sergio <Sergio@192.168.0.13>
2016-10-10 21:02:55 -0500

    Agregado el Main (faltan los parámetros del área).

--- MVCGit1/src/main/Main.java
+++ MVCGit1/src/main/Main.java
@@ -4,10 +4,34 @@ import models.*;
 import views.*;
 import controllers.*;
 
+import javax.swing.JPanel;
+
+import sogc.DataFlow;
+
 public class Main {
 
     public static void main(String[] lrs) {
+        DataFlow dataFlow = new DataFlow();
         
-    }
-    
+        ModelPerimetro modelPerimetro = new ModelPerimetro();
+        ViewPerimetro viewPerimetro = new ViewPerimetro();
+        ControllerPerimetro controllerPerimetro = new ControllerPerimetro(modelPerimetro, viewPerimetro, dataFlow);
+        
+        ModelArea modelArea = new ModelArea();
+        ViewArea viewArea = new ViewArea();
+        ControllerArea controllerArea = new ControllerArea();
+        
+        ModelVolumen modelVolumen = new ModelVolumen();
+        ViewVolumen viewVolumen = new ViewVolumen();
+        ControllerVolumen controllerVolumen = new ControllerVolumen(modelVolumen, viewVolumen, dataFlow);
+        
+        JPanel paneArray[] = new JPanel[3];
+        paneArray[0] = viewPerimetro;
+        paneArray[1] = viewArea;
+        paneArray[2] = viewVolumen;
+        
+        MainModel mainModel = new MainModel();
+        MainView mainView = new MainView();
+        MainController mainController = new MainController(mainModel, mainView, paneArray);
+    }   
 }
