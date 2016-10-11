f5850f0834e293be624e347e5d7ffbb8be2fb8c2
Sergio <Sergio@192.168.0.13>
2016-10-10 20:00:54 -0500

    Agregados Modelo, Vista y Controlador para calcular perímetros.

--- MVCGit1/src/controllers/ControllerPerimetro.java
+++ MVCGit1/src/controllers/ControllerPerimetro.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
 /**
  *
- * @author BLABPC23
+ * @authors Luis y Sergio
  */
-public class ControllerPerimetro {
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import models.ModelPerimetro;
+import views.ViewPerimetro;
+
+import sogc.DataFlow;
+
+public class ControllerPerimetro implements ActionListener {
+    ModelPerimetro modelPerimetro;
+    ViewPerimetro viewPerimetro;
     
+    DataFlow dataFlow;
+
+    public ControllerPerimetro(ModelPerimetro modelPerimetro, ViewPerimetro viewPerimetro, DataFlow dataFlow) {
+        this.modelPerimetro = modelPerimetro;
+        this.viewPerimetro = viewPerimetro;
+        this.dataFlow = dataFlow;
+        
+        this.viewPerimetro.jb_calcularCirculo.addActionListener(this);
+        this.viewPerimetro.jb_calcularCuadrado.addActionListener(this);
+        this.viewPerimetro.jb_calcularTriangulo.addActionListener(this);
+        
+        initView();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent a) {
+        if(a.getSource() == viewPerimetro.jb_calcularCirculo)
+            perimetroCirculo();
+        else if(a.getSource() == viewPerimetro.jb_calcularCuadrado)
+            perimetroCuadrado();
+        else if(a.getSource() == viewPerimetro.jb_calcularTriangulo)
+            perimetroTriangulo();
+    }
+    
+    public void initView() {
+        modelPerimetro.getDiametro();
+        modelPerimetro.getLado();
+        modelPerimetro.getAngulo();
+    }
+    
+    public void perimetroCirculo() {
+        Double diametro = Double.parseDouble(viewPerimetro.jtf_diametro.getText());
+        viewPerimetro.jtf_resultadoCirculo.setText("" + modelPerimetro.perimetroCirculo(diametro));
+        dataFlow.stringToDouble(diametro.toString());
+    }
+    
+    public void perimetroCuadrado() {
+        Double lado = Double.parseDouble(viewPerimetro.jtf_lado.getText());
+        viewPerimetro.jtf_resultadoCuadrado.setText("" + modelPerimetro.perimetroCuadrado(lado));
+        dataFlow.stringToDouble(lado.toString());
+    }
+    
+    public void perimetroTriangulo() {
+        Double angulo = Double.parseDouble(viewPerimetro.jtf_angulo.getText());
+        viewPerimetro.jtf_resultadoTriangulo.setText("" + modelPerimetro.perimetroTriangulo(angulo));
+        dataFlow.stringToDouble(angulo.toString());
+    }
 }
--- MVCGit1/src/models/ModelPerimetro.java
+++ MVCGit1/src/models/ModelPerimetro.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
  *
- * @author BLABPC23
+ * @authors Luis y Sergio
  */
+
 public class ModelPerimetro {
+    double diametro;
+    double lado;
+    double angulo;
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(double diametro) {
+        this.diametro = diametro;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
     
+    public double perimetroCirculo(double diametro) {
+        double perimetro = (3.1416 * diametro);
+        return perimetro;
+    }
+    
+    public double perimetroCuadrado(double lado) {
+        double perimetro = (4 * lado);
+        return perimetro;
+    }
+    
+    public double perimetroTriangulo(double angulo) {
+        double perimetro = (3 * angulo);
+        return perimetro;
+    }
 }
